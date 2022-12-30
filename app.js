e1970611756defb6da7a81a87c32f185cfa79df4
Slof <slof@hey.com>
2022-12-30 15:22:49 -0600

    Add flash messages

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+var flash = require("express-flash");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+
+var sessionStore = new session.MemoryStore();
 
 var app = express();
 
@@ -16,7 +20,17 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("secret"));
+app.use(
+  session({
+    cookie: { maxAge: 60000 },
+    store: sessionStore,
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -39,4 +53,3 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
-
--- routes/index.js
+++ routes/index.js
@@ -2,30 +2,49 @@ var express = require("express");
 var router = express.Router();
 
 const { PrismaClient } = require("@prisma/client");
-const {
-  NotFoundError,
-  PrismaClientKnownRequestError,
-} = require("@prisma/client/runtime");
 const prisma = new PrismaClient();
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  let _todos = await prisma.todo.findMany();
-  res.render("index", { title: "Express", todos: _todos });
+  const todos = await prisma.todo.findMany();
+  res.render("index", {
+    title: "Express",
+    todos: todos,
+  });
 });
 
-router.get("/todo/:id", async function (req, res, next) {
-  let todo = await prisma.todo.findUnique({
-    where: {
-      id: +req.params.id,
+router.post("/", async function (req, res, next) {
+  let name = req.body.name;
+  if (!name) {
+    req.flash("error", "A name is required!");
+    res.redirect("/");
+    return;
+  }
+
+  const todo = await prisma.todo.create({
+    data: {
+      name: req.body.name,
+      complete: false,
     },
   });
 
-  if (!todo) {
-    res.json({ status: 404 });
-  }
-
-  res.render("show", { todo: todo });
+  req.flash("success", "New todo created!");
+  res.redirect("/");
 });
+
+// router.get("/todo/:id", async function (req, res, next) {
+//   let todo = await prisma.todo.findUnique({
+//     where: {
+//       id: +req.params.id,
+//     },
+//   });
+
+//   if (!todo) {
+//     next();
+//     return;
+//   }
+
+//   res.render("show", { todo: todo });
+// });
 
 module.exports = router;
